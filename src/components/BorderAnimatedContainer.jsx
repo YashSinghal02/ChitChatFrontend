8c70147bc93dbfbb673a3c0062e171194db771f2
YashSinghal02 <yash20singhal@gmail.com>
2026-08-29 10:37:42 +0530

    remove the animate-border

--- src/components/BorderAnimatedContainer.jsx
+++ src/components/BorderAnimatedContainer.jsx
@@ -7,7 +7,6 @@ function BorderAnimatedContainer({ children }) {
         border-2 border-transparent
         overflow-hidden
         flex
-        
         [background:linear-gradient(45deg,#1c1735,#2d2154_50%,#1c1735)_padding-box,conic-gradient(from_var(--border-angle),rgb(116_93_192_/_0.3)_80%,#825bf1_86%,#8864ee_90%,#7f5be6_94%,rgb(116_93_192_/_0.3))_border-box]
       "
     >
